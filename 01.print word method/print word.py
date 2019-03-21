e146dc1ee01ae44b505e65c7ea28a0d3708e0f10
seodea <46041493+seodea@users.noreply.github.com>
2019-03-21 14:04:38 +0900

    print word methods

---
+++ 01.print word method/print word.py
@@ -0,0 +1,15 @@
+# only on cmd
+'Basic Python'
+"Basic Python"
+
+# both on cmd or script
+print('Basic Python')
+print("Basic Python")
+
+test1 = "Basic Python"
+print(test1)
+
+test2 = 'Basic Python'
+print(test2)
+
+
