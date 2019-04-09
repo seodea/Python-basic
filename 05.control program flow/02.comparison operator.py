44a48680384ccc74778bf95c0059092c0f23f777
seodea <46041493+seodea@users.noreply.github.com>
2019-04-09 14:04:10 +0900

    how to control program flow

---
+++ 05.control program flow/02.comparison operator.py
@@ -0,0 +1,47 @@
+# comparison operator
+
+# ==
+# If the data on both sides are the same, It's True. Otherwise, It's False. 
+a = 30
+a == 30
+a == 40
+
+# !=
+# If the data on both sides are the different, It's True. Otherwise, It's False.
+a = 'Hello'
+a != 'Hello'
+a != '안녕'
+
+# >
+# If the data on the left is more than the date on the right, It's True. Otherwise, It's False.
+
+a = 30
+a > 20
+a > 40
+
+# >=
+# If the data on the left is same or more than the date on the right, It's True. Otherwise, It's False.
+
+a = 30
+a >= 20
+a >= 30
+a >= 40
+
+# <
+# If the data on the right is more than the date on the left, It's True. Otherwise, It's False.
+
+a = 30
+a < 40
+a < 20
+
+# <=
+# If the data on the right is same or more than the date on the left, It's True. Otherwise, It's False.
+
+a = 30
+a <= 40
+a <= 30
+a <= 20
+
+
+
+
