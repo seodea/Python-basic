ce29cb71527375451e88c0da58c59db185162665
seodea <46041493+seodea@users.noreply.github.com>
2019-04-10 11:58:36 +0900

    how to control program flow

---
+++ 05.control program flow/03.If method.py
@@ -0,0 +1,40 @@
+# If method
+
+# If you want to make a good program flow, you have to use the 'If method'
+# If method is same if grammer in english.
+
+# I'll make a ealier program to use the If method.
+# If someone wirte the number, the number will divide by three.
+# But someone wirte a '0' numver, this program will stop and push the error message.
+
+print('Write the number : ')
+a = int(input())
+
+if a == 0 :
+    print('0 can not use the number')
+else :
+    print ('3 /', a, '=', 3/a)
+
+# Also you can use the 'else if' after 'if method'.
+
+Print('Write the day(Monday to sunday) : ')
+day = input()
+
+if day == 'Monday' :
+    print('MON')
+elif day == 'Tuesday' :
+    print('TUE')
+elif day == 'Wednesday' :
+    print('WED')
+elif day == 'Thursday' :
+    print('THU')
+elif day == 'Friday' :
+    print('FRI')
+elif day == 'Saturday' :
+    print('SAT')
+elif day == 'Sunday' :
+    print('SUN')
+else :
+    print('It is wrong day')
+
+    
---
+++ 05.control program flow/04.while and for method.py
@@ -0,0 +1,73 @@
+# While, for method (Loop Statement)
+
+# This 'While method' will help you if you want to make a loop program until data is True.
+
+print('How many time do you want to repeat it? : ')
+limit = int(input())
+
+count = 0
+while count < limit :
+    count = count + 1
+    print('{0} time repeat.'.format(count))
+
+print('End')
+
+# Also you can use the 'If method' in 'while method'
+# And you can make a 'Infinite Loop' by 'while method'
+
+while True:
+    print('Do you want to reapeat? [Yes/No]')
+    answer = input()
+
+    if answer == 'Yes' :
+        print('Repeat')
+
+    elif answer == 'No' :
+        print('Stop to repeat')
+        break
+
+    else :
+        print('It is wrong answer')
+
+# This 'for method' is little bit different.
+# 'For method' is repeated using sequence like 'list', 'tuple', 'string'.
+
+for i in (1, 2, 3):
+    pring(i)
+
+# That sequence is three tuple. So That for program can repeat three.
+
+for list in ['Hello', 'Python', 'Programer'] :
+    print(list)
+
+for string in 'Hello Python Programer' :
+    print(string)
+
+# But Many programer use the 'range' for sequence in for program.
+
+for i in range(0,5,1) :
+    print(i)
+
+# The 0 number means start value.
+# The 5 number means stop value.
+# The 1 number means increasing value.
+
+for i in range(0,5):
+    print(i)
+
+# The increasing value is not necessary.
+
+for i in range(5):
+    print(i)
+
+# Also the start value is not necessary.
+
+# For example, you can make a program of add the number 1 to 10.
+
+a = 0
+for i in range(1,10):
+    a = a + i
+    print(a)
+
+
+    
